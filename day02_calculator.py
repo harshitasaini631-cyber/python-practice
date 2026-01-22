8e0dadff32113a5cab4c8577672d47161faca9a8
Harshita saini <harshitasaini631@gmail.com>
2026-01-22 21:28:25 +0530

    simple calculator

---
+++ day02_calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+sum = num1 + num2
+difference = num1 - num2
+product = num1 * num2 
+
+print(f"sum:{sum} \nDifference:{difference} \nProduct:{product}")
