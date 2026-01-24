920736129f9db7401acb89e5e94a5f5a9b0b488b
Harshita saini <harshitasaini631@gmail.com>
2026-01-24 22:07:04 +0530

    comparing numbers and printer the larger one using conditional statement

---
+++ number_comparison.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the number:"))
+num2 = int(input("Enter the number:"))
+
+if num1 > num2:
+    print(num1)
+elif num2 > num1:
+    print(num2)
+else:
+    print("Numbers are equal")        
