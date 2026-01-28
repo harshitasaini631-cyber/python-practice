46c5ef350dfeaccfdb3e8ff93e0796dbba7d06d1
Harshita saini <harshitasaini631@gmail.com>
2026-01-28 22:20:28 +0530

    Basics of dictionary

---
+++ dictionary-basics.py
@@ -0,0 +1,5 @@
+dict1 = {"name":"Harshita","age":20, "role":"engineer"}
+print("Name:",dict1["name"])
+print("Age:",dict1["age"])
+print("Role:",dict1["role"])
+print(f"My name is {dict1['name']} and I am {dict1['age']} years old.")
