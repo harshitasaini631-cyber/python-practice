032a3c9e3fbbe98c6289fe0109a1d2cb6414b891
Harshita saini <harshitasaini631@gmail.com>
2026-03-01 15:32:08 +0530

    Performing linear search in an array

---
+++ DSA-python/Arrays/p5.py
@@ -0,0 +1,9 @@
+#Searching
+#Linear search
+
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return -1
+print(linear_search([1,2,3],3))
