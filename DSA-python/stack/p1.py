f8aa79794e1f191a60d1650fc8268f268d6dc9c4
Harshita saini <harshitasaini631@gmail.com>
2026-03-11 22:02:48 +0530

    Adding pop operation in stack

--- DSA-python/stack/p1.py
+++ DSA-python/stack/p1.py
@@ -4,7 +4,13 @@ class Stack:
 
     def push(self,item):
         self.stack.append(item)
-        print(item,"append")    
+        print(item,"append")  
+
+    def pop(self):
+        if len(self.stack)==0:
+            print("stack underflow")      
+        else:
+            (self.stack.pop(),"element popped")    
 
     def display(self):
         print(self.stack)    
@@ -13,6 +19,8 @@ class Stack:
 s = Stack()
 s.push(10)       
 s.push(20)       
-s.push(30)   
+s.push(30)  
+
+s.pop()
 
 s.display()
