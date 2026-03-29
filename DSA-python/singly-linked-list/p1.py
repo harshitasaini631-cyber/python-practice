901136e8c344b841fde38a5426d7d8af9282dea7
Harshita saini <harshitasaini631@gmail.com>
2026-03-29 20:43:17 +0530

    Inserting element at the beginning

--- DSA-python/singly-linked-list/p1.py
+++ DSA-python/singly-linked-list/p1.py
@@ -6,10 +6,19 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+    
 
+    #traversing thorugh a linked list
     def traverse(self):
         temp = self.head
         while temp:
             print(temp.data, end=" -> ")
             temp = temp.next
-        print("None")        
+        print("None")       
+
+
+    #insert at beginning
+    def insert_beginning(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
