694a02de183cc2710c7fdaf4a3807e6bca7de194
Harshita saini <harshitasaini631@gmail.com>
2026-04-03 14:33:49 +0530

    Creating doubly linked list

--- DSA-python/doubly-linked-list/p1.py
+++ DSA-python/doubly-linked-list/p1.py
@@ -1,6 +1,12 @@
-#creating a node
+#creating node
 class DNode:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.prev = None
         self.next = None
+
+#creating doubly linked list
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
