275d9745d9945dd3829bc360f24e1de381a78346
Harshita saini <harshitasaini631@gmail.com>
2026-04-04 19:44:45 +0530

    Insert at beginning in doubly linked list

--- DSA-python/doubly-linked-list/p1.py
+++ DSA-python/doubly-linked-list/p1.py
@@ -1,4 +1,4 @@
-#creating node
+#creating a node
 class DNode:
     def __init__(self, data):
         self.data = data
@@ -10,10 +10,21 @@ class DoublyLinkedList:
     def __init__(self):
         self.head = None
 
-#traversing in doubly linked list
+#traversing in a dpoubly linked list
     def traverse_forward(self):
         temp = self.head
         while temp:
             print(temp.data, end=" <-> ")
             temp = temp.next
         print("None")
+
+#insert at beginnig
+    def insert_at_beginning(self, data):
+        new_node = DNode(data)
+
+        if self.head is not None:
+            new_node.next = self.head
+            self.head.prev = new_node
+
+        self.head = new_node
+      
